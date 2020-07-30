b84d3f6f115495cff7bac89ce748d74b0e2acabf
Naquino14 <naquino14@outlook.com>
2020-07-30 13:07:09 -0400

    Hello World! | Camera Controller

---
+++ Scripts/PlayerLook.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLook : MonoBehaviour
+{
+    public float mouseSensitivity = 100f;
+    public float fov = 60;
+
+    public Transform playerBody;
+
+    private float xRotation = 0;
+
+    public Camera camera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        camera.fieldOfView = fov;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90, 90);
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0, 0);
+        playerBody.Rotate(Vector3.up * mouseX);
+
+        
+    }
+}
