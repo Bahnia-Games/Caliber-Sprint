b3b5b8407158e2eca15e3757167d65814de6df99
Naquino14 <naquino14@outlook.com>
2020-08-03 10:22:10 -0400

    Update RBMovementController.cs

--- Scripts/RBMovementController.cs
+++ Scripts/RBMovementController.cs
@@ -7,25 +7,59 @@ public class RBMovementController : MonoBehaviour
 
     public float speed = 5f;
     Rigidbody rb;
+    public Vector3 movement;
+
+    public LayerMask groundMask;
+    public float checkRad;
+    public Transform groundCheck;
+    private bool isGrounded;
+    public float jumpForce;
+    int amtJump = 0;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        //unused
+        rb = GetComponent<Rigidbody>();
     }
 
     private void Awake()
     {
-        rb = GetComponent<Rigidbody>();
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        float xAx = Input.GetAxisRaw("Horizontal");
-        float zAx = Input.GetAxisRaw("Vertical");
+        movement = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
 
-        rb.velocity = new Vector3(xAx, rb.velocity.y, zAx) * speed * Time.deltaTime;
+        isGrounded = Physics.CheckSphere(groundCheck.position, checkRad, groundMask);
 
+        if (isGrounded)
+        {
+            amtJump = 0;
+        }
+
+        if (Input.GetButton("Jump") && isGrounded && amtJump < 2)
+        {
+            jump();
+        }
+
+    }
+
+    private void FixedUpdate()
+    {
+        movePlayer(movement);
+    }
+
+    void movePlayer(Vector3 direction)
+    {
+        rb.AddRelativeForce(direction * speed * Time.deltaTime);
+    }
+
+    void jump()
+    {
+        rb.AddForce(Vector3.up * jumpForce);
+        amtJump++;
     }
 }
