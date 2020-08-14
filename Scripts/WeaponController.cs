bd4c9888fb744f58cd5b1233a2967256c211f2b7
Naquino14 <37340195+Naquino14@users.noreply.github.com>
2020-08-13 20:12:57 -0400

    Weapon Controller v1.5
    yeah

--- Scripts/WeaponController.cs
+++ Scripts/WeaponController.cs
@@ -1,8 +1,8 @@
-﻿using UnityEngine;
+using UnityEngine;
 
 public class WeaponController : MonoBehaviour
 {
-    public int selectedWeapon = 2;
+    public int selectedWeapon = 0;
 
     // Start is called before the first frame update
     void Start()
@@ -13,7 +13,9 @@ public class WeaponController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetAxis("Mouse ScrollWheel") > 0f)
+        int previousSelectedWeapon = selectedWeapon;
+
+        if(Input.GetAxis("Mouse ScrollWheel") > 0f)
         {
             if(selectedWeapon >= transform.childCount - 1)
             {
@@ -27,14 +29,74 @@ public class WeaponController : MonoBehaviour
 
         if (Input.GetAxis("Mouse ScrollWheel") < 0f)
         {
-            if(selectedWeapon <= 0)
+            if (selectedWeapon <= 0)
             {
-                selectedWeapon = transform.childCount -1;
-            } else
+                selectedWeapon = transform.childCount - 1;
+            }
+            else
             {
                 selectedWeapon--;
             }
-            
+
+        }
+        #region le repetitive if statements have arrived
+
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+                selectedWeapon = 0;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha2) && transform.childCount >= 2)
+        {
+            selectedWeapon = 1;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha3) && transform.childCount >= 3)
+        {
+            selectedWeapon = 2;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha4) && transform.childCount >= 4)
+        {
+            selectedWeapon = 3;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha5) && transform.childCount >= 5)
+        {
+            selectedWeapon = 4;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha6) && transform.childCount >= 6)
+        {
+            selectedWeapon = 5;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha7) && transform.childCount >= 7)
+        {
+            selectedWeapon = 6;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha8) && transform.childCount >= 8)
+        {
+            selectedWeapon = 7;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha9) && transform.childCount >= 9)
+        {
+            selectedWeapon = 8;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha0) && transform.childCount >= 10)
+        {
+            selectedWeapon = 9;
+        }
+
+        #endregion
+
+
+        if (previousSelectedWeapon != selectedWeapon)
+        {
+            SelectWeapon();
         }
     }
 
@@ -43,7 +105,7 @@ public class WeaponController : MonoBehaviour
         int i = 0;
         foreach(Transform weapon in transform)
         {
-            if (i == selectedWeapon)
+            if(i == selectedWeapon)
             {
                 weapon.gameObject.SetActive(true);
             } else
@@ -51,7 +113,6 @@ public class WeaponController : MonoBehaviour
                 weapon.gameObject.SetActive(false);
             }
             i++;
-
         }
     }
 }
