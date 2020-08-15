6d7ee9069e8f1fdf264c66cb75c505367cd2c247
Naquino14 <37340195+Naquino14@users.noreply.github.com>
2020-08-15 14:41:40 -0400

    Gun Framework v2.0
    works like a charm! Note: fix bugs in animator (unwanted delays and unsynced firing.)
    THIS CODE WORKS ON ANY GUN ANYWHERE IN ANY GAME!

---
+++ Scripts/GunController.cs
@@ -0,0 +1,164 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunController : MonoBehaviour
+{
+
+    //public string currentWeapon;
+    public float reloadDelay = 1.0f;
+    public float fireDelay = 0.1f;
+    public float damage = 1f;
+    public float range = 1f;
+    public int magSize = 1;
+    public float impactForce = 1f;
+    private int currentAmmo;
+    public string fireMode = "SemiAuto";
+    public Camera camera;
+    public Animator animator;
+
+    public ParticleSystem muzzleFlash;
+    public ParticleSystem muzzleFlash2;
+    public ParticleSystem muzzleFlash3;
+    public Light muzzleLight;
+    public GameObject impactEffect;
+    private int layerMask = 1 << 8;
+
+    private bool isFire;
+    private bool isReload;
+
+    private bool isEmpty;
+    private bool isTac;
+
+    private void Awake()
+    {
+        
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (fireMode == null) //Check if no fire mode is entered. Default is Semi Auto
+        {
+            fireMode = "SemiAuto";
+        }
+        currentAmmo = magSize;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        #region firing
+
+        if(Input.GetKeyDown(KeyCode.Mouse0) && fireMode == "SemiAuto" && isReload == false) //Fire everytime mouse is clicked (Semi Auto)
+        {
+            StartCoroutine(Shoot());
+        }
+        if(Input.GetKey(KeyCode.Mouse0) && fireMode == "Auto" && isReload == false) //Fire every frame mouse is held (Auto)
+        {
+            StartCoroutine(Shoot());
+        }
+
+        #endregion
+
+        #region reloading
+
+        if (Input.GetKeyDown(KeyCode.R) && isFire == false && currentAmmo >= 1 && currentAmmo != magSize) // if the gun isnt firing and r is pressed, determine reload type
+        {
+                // does the magazoine have at least 1 bullet and is less than the max ammo size?
+                isTac = true; // tactical reload
+                StartCoroutine(Reload());
+        }
+
+        if (currentAmmo <= 0) // is the current ammo 0?
+        {
+            isEmpty = true; //mag empty reload
+            StartCoroutine(Reload());
+        }
+
+        #endregion
+
+        Debug.Log(currentAmmo);
+    }
+
+    IEnumerator Shoot()
+    {
+        isFire = true;
+        currentAmmo--; //subtract 1 bullet
+        animator.SetBool("isFire", true);
+
+        #region muzzle flashing
+
+        if (muzzleFlash != null)
+        {
+            muzzleFlash.Play();
+        }
+        
+        if(muzzleLight != null)
+        {
+            muzzleLight.gameObject.SetActive(true);
+        }
+
+        if(muzzleFlash2 != null)
+        {
+            muzzleFlash2.Play();
+        }
+
+        if(muzzleFlash3 != null)
+        {
+            muzzleFlash3.Play();
+        }
+
+        #endregion
+
+        RaycastHit hit;
+        if(Physics.Raycast(camera.transform.position, camera.transform.forward, out hit, range, layerMask))
+        {
+            target target = hit.transform.GetComponent<target>();
+
+            if(target != null)
+            {
+                target.takeDamage(damage);
+            }
+
+            if(hit.rigidbody != null)
+            {
+                hit.rigidbody.AddForce(-hit.normal * impactForce);
+            }
+
+            GameObject impactGameObj = Instantiate(impactEffect, hit.point, Quaternion.LookRotation(hit.normal));
+            Destroy(impactGameObj, 1f);
+        }
+
+        yield return new WaitForSeconds(fireDelay); //wait for fire delay
+
+        muzzleLight.gameObject.SetActive(false);
+        animator.SetBool("isFire", false);
+        isFire = false;
+    }
+
+    IEnumerator Reload()
+    {
+        isReload = true;
+
+        if (isEmpty)
+        {
+            animator.SetBool("isReload", true);
+        }
+        if (isTac)
+        {
+            animator.SetBool("isTacReload", true);
+        }
+
+        
+
+        yield return new WaitForSeconds(reloadDelay); //wait for reload delay
+
+        animator.SetBool("isReload", false);
+        animator.SetBool("isTacReload", false);
+        isReload = false;
+        currentAmmo = magSize; //refil mag
+        isTac = false;
+        isEmpty = false;
+    }
+}
