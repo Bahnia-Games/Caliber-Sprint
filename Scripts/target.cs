6027469af09e83b200179116f2053d90527b6a2f
Naquino14 <naquino14@outlook.com>
2020-08-29 12:05:03 -0400

    Start adding playermodel capability

--- Scripts/target.cs
+++ Scripts/target.cs
@@ -5,6 +5,22 @@ public class target : MonoBehaviour
     public float health = 100f;
     public string deathMethod;
 
+    public Transform head;
+    public Transform spine;
+    public Transform rightArm;
+    public Transform rightForeArm;
+    public Transform rightHand;
+    public Transform leftArm;
+    public Transform leftForeArm;
+    public Transform leftHand;
+    public Transform torso;
+    public Transform rightThigh;
+    public Transform rightAnkle;
+    public Transform rightFoot;
+    public Transform leftThigh;
+    public Transform leftAnkle;
+    public Transform leftFoot;
+
     public void takeDamage(float amount)
     {
         health -= amount;
