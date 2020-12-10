a0bceb1dfd412993cd3261532193d189cd1b3b55
Naquino14 <naquino14@outlook.com>
2020-12-10 13:25:55 -0500

    fixed elapsed time

--- Scripts/Misc/DRP.cs
+++ Scripts/Misc/DRP.cs
@@ -8,11 +8,11 @@ public class DRP : MonoBehaviour
     private string detail;
     [SerializeField]
     private string state;
-    [SerializeField]
-    long elapsedTime;
+    private long elapsedTime;
 
     private void Start()
     {
+        elapsedTime = (long)DateTime.UtcNow.Subtract(new DateTime(1970, 1, 1)).TotalSeconds;
         PresenceManager.UpdatePresence(detail, state, elapsedTime);
     }
 }
