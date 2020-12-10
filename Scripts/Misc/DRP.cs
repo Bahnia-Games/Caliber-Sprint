60c07dbdbed0b14ddd1035d6aea94a6a1c277883
Naquino14 <naquino14@outlook.com>
2020-12-10 09:28:30 -0500

    added Discord Rich Presence support
    helb unity is haveing stroke

---
+++ Scripts/Misc/DRP.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using DiscordPresence;
+
+public class DRP : MonoBehaviour
+{
+    [SerializeField]
+    private string detail;
+    [SerializeField]
+    private string state;
+
+    private void Start()
+    {
+        PresenceManager.UpdatePresence(detail, state);
+    }
+}
