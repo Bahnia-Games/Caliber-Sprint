3630c779faf4937eca58b3800087dab949f25a2d
Naquino14 <naquino14@outlook.com>
2021-06-02 09:57:03 -0400

    untested menu

--- Scripts/Menu/KeybindsMenu.cs
+++ Scripts/Menu/KeybindsMenu.cs
@@ -27,10 +27,67 @@ namespace Assets.Git.Scripts.Menu
         private InputHandler.Inputs currentControl;
         private void Start()
         {
+            SetJumpButton.GetComponentInChildren<TMP_Text>().text = InputHandler.JumpKC.ToString();
             SetJumpButton.onClick.AddListener(delegate
             {
                 currentControl = InputHandler.Inputs.Jump;
                 StartCoroutine(AllowSet(SetJumpButton, currentControl));
+            });
+
+            SetCrouchButton.GetComponentInChildren<TMP_Text>().text = InputHandler.CrouchKC.ToString();
+            SetCrouchButton.onClick.AddListener(delegate
+            {
+                currentControl = InputHandler.Inputs.Crouch;
+                StartCoroutine(AllowSet(SetCrouchButton, currentControl));
+            });
+
+            SetFireGrappleButton.GetComponentInChildren<TMP_Text>().text = InputHandler.FireGrappleKC.ToString();
+            SetFireGrappleButton.onClick.AddListener(delegate
+            {
+                currentControl = InputHandler.Inputs.FireGrapple;
+                StartCoroutine(AllowSet(SetFireGrappleButton, currentControl));
+            });
+
+            SetFireButton.GetComponentInChildren<TMP_Text>().text = InputHandler.FireKC.ToString();
+            SetFireButton.onClick.AddListener(delegate
+            {
+                currentControl = InputHandler.Inputs.Fire;
+                StartCoroutine(AllowSet(SetAimButton, currentControl));
+            });
+
+            SetAimButton.GetComponentInChildren<TMP_Text>().text = InputHandler.AimKC.ToString();
+            SetAimButton.onClick.AddListener(delegate
+            {
+                currentControl = InputHandler.Inputs.Aim;
+                StartCoroutine(AllowSet(SetAimButton, currentControl));
+            });
+
+            SetQuickSwitchButton.GetComponentInChildren<TMP_Text>().text = InputHandler.QuickSwitchKC.ToString();
+            SetQuickSwitchButton.onClick.AddListener(delegate
+            {
+                currentControl = InputHandler.Inputs.QuickSwitch;
+                StartCoroutine(AllowSet(SetQuickSwitchButton, currentControl));
+            });
+
+            SetUnEquipButton.GetComponentInChildren<TMP_Text>().text = InputHandler.UnEquipKC.ToString();
+            SetUnEquipButton.onClick.AddListener(delegate
+            {
+                currentControl = InputHandler.Inputs.UnEquip;
+                StartCoroutine(AllowSet(SetUnEquipButton, currentControl));
+            });
+
+            SetQuickMeleeButton.GetComponentInChildren<TMP_Text>().text = InputHandler.QuickMeleeKC.ToString();
+            SetQuickMeleeButton.onClick.AddListener(delegate
+            {
+                currentControl = InputHandler.Inputs.QuickMelee;
+                StartCoroutine(AllowSet(SetQuickMeleeButton, currentControl));
+            });
+
+            SetThrowGrenadeButton.GetComponentInChildren<TMP_Text>().text = InputHandler.ThrowGrenadeKC.ToString();
+            SetThrowGrenadeButton.onClick.AddListener(delegate
+            {
+                currentControl = InputHandler.Inputs.ThrowGrenade;
+                StartCoroutine(AllowSet(SetThrowGrenadeButton, currentControl));
             });
         }
 
@@ -45,7 +102,7 @@ namespace Assets.Git.Scripts.Menu
         private IEnumerator AllowSet(Button button, InputHandler.Inputs control)
         {
             checkinFlag = true;
-            TMP_Text txt = button.GetComponent<TMP_Text>();
+            TMP_Text txt = button.GetComponentInChildren<TMP_Text>();
             txt.text = "Press any key.";
             yield return new WaitForSeconds(controlSetTime);
             checkinFlag = false;
